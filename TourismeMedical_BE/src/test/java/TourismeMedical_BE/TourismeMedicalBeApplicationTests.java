93f2afe43648426376e1290b62fd4dde08beb6c2
hadil.HM <88782173+HM-Hadil@users.noreply.github.com>
2023-03-04 14:04:27 +0100

    Add files via upload

---
+++ TourismeMedical_BE/src/test/java/TourismeMedical_BE/TourismeMedicalBeApplicationTests.java
@@ -0,0 +1,13 @@
+package TourismeMedical_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourismeMedicalBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
