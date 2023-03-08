ea5c5fb75425e7fe9d19c60c2c20f891bca25ca3
El Amri, Mohamed (EXTERN: Novobit) <m.el.amri@novobit.eu>
2023-03-08 23:49:13 +0100

    swagger docs config updated

--- src/main/java/com/innovup/meto/config/SwaggerDocumentationConfiguration.java
+++ src/main/java/com/innovup/meto/config/SwaggerDocumentationConfiguration.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
@@ -12,8 +14,9 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.*;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-// @Configuration
-// @ConditionalOnExpression(value = "${swagger.enabled:false}")
+import java.time.LocalDate;
+
+@Configuration
 @EnableSwagger2
 public class SwaggerDocumentationConfiguration {
 
@@ -37,11 +40,15 @@ public class SwaggerDocumentationConfiguration {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .useDefaultResponseMessages(false)
                 .select()
+                .paths(PathSelectors.any())
                 .apis(RequestHandlerSelectors.basePackage("com.innovup.meto.controller"))
                 .build()
-                .apiInfo(apiInfo());
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)
+                .directModelSubstitute(LocalDate.class, String.class)
+                .genericModelSubstitutes(ResponseEntity.class)
+                ;
     }
 
     @Bean
@@ -62,4 +69,5 @@ public class SwaggerDocumentationConfiguration {
                 .validatorUrl(null)
                 .build();
     }
+
 }
