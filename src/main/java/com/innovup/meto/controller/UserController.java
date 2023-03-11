6381f008620b07bff137da8c66855b55b83be714
HM-Hadil <hadilhammami670@gmail.com>
2023-03-11 13:14:51 +0100

    added post doctor and patient endpoint.

--- src/main/java/com/innovup/meto/controller/UserController.java
+++ src/main/java/com/innovup/meto/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.innovup.meto.controller;
 
 import com.innovup.meto.request.CreateAdminRequest;
+import com.innovup.meto.request.CreateDoctorRequest;
+import com.innovup.meto.request.CreatePatientRequest;
 import com.innovup.meto.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,6 +28,22 @@ public class UserController implements UserApi {
     public ResponseEntity<?> createAdmin(@RequestBody CreateAdminRequest request) {
         log.info("Endpoint '/.../' (POST) called - request = {}", request);
         var response = userService.createNewAdmin(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @Override
+    @PostMapping(path = "/patient", produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> createPatient(@RequestBody CreatePatientRequest request) {
+        log.info("Endpoint '/.../' (POST) called - request = {}", request);
+        var response = userService.createNewPatient(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @Override
+    @PostMapping(path = "/doctor", produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> createDoctor(@RequestBody CreateDoctorRequest request) {
+        log.info("Endpoint '/.../' (POST) called - request = {}", request);
+        var response = userService.createNewDoctor(request);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
--- src/main/java/com/innovup/meto/service/UserService.java
+++ src/main/java/com/innovup/meto/service/UserService.java
@@ -4,6 +4,8 @@ import com.innovup.meto.entity.User;
 import com.innovup.meto.enums.Role;
 import com.innovup.meto.repository.UserRepository;
 import com.innovup.meto.request.CreateAdminRequest;
+import com.innovup.meto.request.CreateDoctorRequest;
+import com.innovup.meto.request.CreatePatientRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -30,6 +32,42 @@ public class UserService {
         /*
         ObjectMapper.map(user, createUserResult)
          */
+        user = userRepository.save(user);
+        return user;
+    }
+
+    public User createNewPatient(CreatePatientRequest request) {
+        var user = User.builder()
+                .withId(UUID.randomUUID())
+                .withFirstname(request.getFirstname())
+                .withLastname(request.getLastname())
+                .withEmail(request.getEmail())
+                .withPassword(request.getPassword())
+                .withRole(Role.PATIENT)
+                .withCreatedOn(LocalDate.now())
+                .build();
+        user = userRepository.save(user);
+        return user;
+    }
+
+    public User createNewDoctor(CreateDoctorRequest request) {
+
+        var user = User.builder()
+                .withId(UUID.randomUUID())
+                .withFirstname(request.getFirstname())
+                .withLastname(request.getLastname())
+                .withEmail(request.getEmail())
+                .withPassword(request.getPassword())
+                .withVille(request.getVille())
+                .withSpecialite(request.getSpecialite())
+                .withSexe(request.getSexe())
+                .withExperience(request.getExperience())
+                .withParcours(request.getParcours())
+                .withImage(request.getImage())
+                .withAdresse(request.getAdresse())
+                .withRole(Role.DOCTOR)
+                .withCreatedOn(LocalDate.now())
+                .build();
         user = userRepository.save(user);
         return user;
     }
